82ba81b4f576cddc3021e1d1986d86668ea29b8d
Marvin Carstensen <mca@hoch2.de>
2025-05-21 14:31:43 +0200

    Fixed vite build errors

--- frontend/src/components/section-cards.tsx
+++ frontend/src/components/section-cards.tsx
@@ -1,17 +1,15 @@
-import { IconTrendingDown, IconTrendingUp } from "@tabler/icons-react"
-
-import { Badge } from "@/components/ui/badge"
 import {
   Card,
-  CardAction,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
 
 export function SectionCards() {
-  const token = localStorage.getItem('token').substring(0, 100) + '...';
+  let token = localStorage.getItem('token')
+  if (token) {
+    token = token.substring(0, 100) + '...';
+  }
 
   return (
     <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-1">
