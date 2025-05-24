875e96a752466ae2ca0b179259c521b87d109e36
Marvin Carstensen <marvin.carstensen@gmail.com>
2025-05-24 15:56:55 +0200

    Added breadcrumbs

--- frontend/src/pages/Home.page.tsx
+++ frontend/src/pages/Home.page.tsx
@@ -1,8 +1,10 @@
+import BreadcrumbsBar from '@/components/BreadcrumbsBar/BreadcrumbsBar';
 import CaseOverview from './CaseOverview.page';
 
 export function HomePage() {
   return (
     <>
+      <BreadcrumbsBar/>
       <CaseOverview />
     </>
   );
