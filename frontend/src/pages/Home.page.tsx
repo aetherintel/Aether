ab7bc53d990f04175b776d2cdb259d8564922317
Marvin Carstensen <marvin.carstensen@gmail.com>
2025-05-25 09:14:00 +0200

    Added dynamics breadcrumbs and fixed links

--- frontend/src/pages/Home.page.tsx
+++ frontend/src/pages/Home.page.tsx
@@ -1,11 +1,9 @@
 import BreadcrumbsBar from '@/components/BreadcrumbsBar/BreadcrumbsBar';
-import CaseOverview from './CaseOverview.page';
 
 export function HomePage() {
   return (
     <>
       <BreadcrumbsBar/>
-      <CaseOverview />
     </>
   );
 }
