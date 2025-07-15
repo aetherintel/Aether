390af88d1501c6b7c2112ffb055a6b65abcc0056
Marvin Carstensen <marvin.carstensen@gmail.com>
2025-07-15 17:29:36 +0200

    Trying to fix "Invalid redirect uri"

--- backend/app/controller/auth_controller.py
+++ backend/app/controller/auth_controller.py
@@ -147,6 +147,8 @@ def send_verification_email(user_id: str, admin_token: str):
     # FIX: Container-interne URL für Admin API
     email_url = f"{os.getenv('KEYCLOAK_BASE_URL')}/admin/realms/HotTopics/users/{user_id}/execute-actions-email"
     
+    print(f"Redirect_uri: {os.getenv("FRONTEND_URL", "http://localhost/")}")
+
     # Query Parameter für Redirect (EXTERN URL!)
     params = {
         # TODO: redirect URL einfügen
