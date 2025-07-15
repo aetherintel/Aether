421a62d2be166b92d4f70c8e1ae25b05f26b8ae1
jan-frederik.felsch <jan-frederik.felsch@team.de>
2025-07-15 18:25:27 +0200

    check is_downloaded

--- backend/app/controller/casefile_controller.py
+++ backend/app/controller/casefile_controller.py
@@ -249,7 +249,7 @@ async def read_casefile(
         messages = await get_messages_with_media(
             owner_id=owner,
             channel_ids=channel_ids,
-            limit=20,
+            limit=100,
         )
 
         messages_with_media = []
